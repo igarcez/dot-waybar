6eb6e112f97d7260b65f738af80ccc79a53c1b80
Ian Garcez <6891905+igarcez@users.noreply.github.com>
2025-11-16 22:07:43 +0100

    add cava again

---
+++ cava.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+bar="▁▂▃▄▅▆▇█"
+dict="s/;//g;"
+
+# creating "dictionary" to replace char with bar
+i=0
+while [ $i -lt ${#bar} ]
+do
+    dict="${dict}s/$i/${bar:$i:1}/g;"
+    i=$((i=i+1))
+done
+
+# read stdout from cava
+cava | while read -r line; do
+    echo $line | sed $dict
+done
